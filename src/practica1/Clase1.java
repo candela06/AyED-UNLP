80ab283375b540e8fbd731797453c40a426c0634
candela silva <silvacande06@gmail.com>
2024-04-02 17:33:34 -0300

    ayed

---
+++ src/practica1/Clase1.java
@@ -0,0 +1,26 @@
+
+package practica1;
+
+
+public class Clase1 {
+    
+    public static void conFor(int a, int b){
+        for (int i=a; i<=b;i++){
+            System.out.println(i);
+        }
+    }
+    
+    public static void conWhile(int a, int b){
+        while (a<=b){
+            System.out.println(a);
+            a++;
+        }
+    }
+    
+    public static void conRecursion(int a, int b){
+        if (a<=b){
+            System.out.println(a);
+            conRecursion(a+1,b);
+        }
+    }
+}
