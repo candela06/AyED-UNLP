81d806e91f636645cee9bfd26a6ca526bec2e272
candela silva <silvacande06@gmail.com>
2024-04-02 17:53:26 -0300

    ejercicios de ayed

--- src/Practica1/Ejercicio1/Clase1.java
+++ src/Practica1/Ejercicio1/Clase1.java
@@ -1,16 +1,16 @@
 
-package practica1;
+package Practica1.Ejercicio1;
 
 
 public class Clase1 {
     
-    public static void conFor(int a, int b){
-        for (int i=a; i<=b;i++){
+    public static void conFor(int a,int b){
+        for(int i=a;i<=b;i++){
             System.out.println(i);
         }
     }
     
-    public static void conWhile(int a, int b){
+    public static void conWhile(int a,int b){
         while (a<=b){
             System.out.println(a);
             a++;
--- src/Practica1/Ejercicio1/Uno.java
+++ src/Practica1/Ejercicio1/Uno.java
@@ -1,6 +1,4 @@
-
-/**
- * 
+/*
  * Escriba tres métodos de clase (static) que reciban por parámetro dos números enteros (tipo
 int) a y b e impriman todos los números enteros comprendidos entre a; b (inclusive), uno por
 cada línea en la salida estándar. Para ello, dentro de una nueva clase escriba un método por
@@ -12,20 +10,21 @@ while).
 Por último, escriba en el método de clase main el llamado a cada uno de los métodos
 creados, con valores de ejemplo. En su computadora, ejecute el programa y verifique
 que se cumple con lo pedido.
-
  */
-package practica1;
+package Practica1.Ejercicio1;
 
-public class PuntoUno {
+public class Uno {
 
-   
     public static void main(String[] args) {
-       System.out.println("con for...");
-       Clase1.conFor(1, 5);
-       System.out.println("con while...");
-       Clase1.conWhile(4, 7);
-       System.out.println("con recursión...");
-       Clase1.conRecursion(7, 12);
+        
+        System.out.println("con for...");
+        Clase1.conFor(1, 5);
+        System.out.println("con while...");
+       Clase1.conWhile(6, 10);
+       System.out.println("con recursion...");
+       Clase1.conRecursion(11, 15);
     }
+    
+    
     
 }
