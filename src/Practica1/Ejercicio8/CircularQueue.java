d75930ce984156f65ae1693e6494b496cf2d1ebe
candela silva <silvacande06@gmail.com>
2024-05-14 20:34:22 -0300

    ejercicio 8

---
+++ src/Practica1/Ejercicio8/CircularQueue.java
@@ -0,0 +1,13 @@
+
+package Practica1.Ejercicio8;
+
+public class CircularQueue<T> extends Queue<T>{
+    public T shift(){
+        if(!super.isEmpty()){
+            T dato = super.dequeue();
+            super.enqueue(dato);
+            return dato;
+        }
+        else return null;
+    }
+}
---
+++ src/Practica1/Ejercicio8/DoubleEndedQueue.java
@@ -0,0 +1,12 @@
+
+package Practica1.Ejercicio8;
+
+public class DoubleEndedQueue<T> extends Queue<T> {
+    public void enqueueFirst(T data){
+        Queue<T> temp = new Queue<T>();
+        
+        while(!super.isEmpty())temp.enqueue(super.dequeue());
+        super.enqueue(data);
+        while (!temp.isEmpty()) super.enqueue(temp.dequeue());
+    }
+}
